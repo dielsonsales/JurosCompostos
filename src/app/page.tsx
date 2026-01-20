75dddcaa0b9e1cb9973728f08819e18f407807c9
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-20 16:51:04 -0300

    Reorganize page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ import Result from "@/components/result/result";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Juros Compostos</h1>
-        <p>Simule a evolução do seu capital considerando aportes mensais e uma taxa recorrente.</p>
-      </div>
-      <div className={styles.cardsContainer}>
-        <Inputs />
-        <Result />
-      </div>
-    </main>
+    <>
+      <main>
+        <div>
+          <h1>Juros Compostos</h1>
+          <p>Simule a evolução do seu capital considerando aportes mensais e uma taxa recorrente.</p>
+        </div>
+        <div className={styles.cardsContainer}>
+          <Inputs />
+          <Result />
+        </div>
+        <div>
+          <h2>Evolução de patrimônio</h2>
+        </div>
+      </main>
+      <footer>
+        <span>Dielson Sales de Carvalho</span>
+      </footer>
+    </>
   );
 }
