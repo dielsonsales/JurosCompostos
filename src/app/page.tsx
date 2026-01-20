bc7150f85d2897ef14face14636f69e1fc7f5318
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-20 17:09:30 -0300

    Create Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 import Inputs from "@/components/inputs/inputs";
 import styles from "./page.module.css";
 import Result from "@/components/result/result";
+import Card from "@/components/card/card";
 
 export default function Home() {
   return (
     <>
-      <main>
+      <main style={{ backgroundColor: "lightGreen" }}>
         <div>
           <h1>Juros Compostos</h1>
           <p>Simule a evolução do seu capital considerando aportes mensais e uma taxa recorrente.</p>
         </div>
         <div className={styles.cardsContainer}>
-          <Inputs />
-          <Result />
+          <Card>
+            <Inputs />
+          </Card>
+          <Card>
+            <Result />
+          </Card>
         </div>
         <div>
           <h2>Evolução de patrimônio</h2>
---
+++ src/components/card/card.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import styles from "./card.module.css";
+
+interface CardProps {
+  children: ReactNode;
+}
+
+export default function Card({ children }: CardProps) {
+  return (
+    <div className={styles.card}>
+      {children}
+    </div>
+  );
+}
