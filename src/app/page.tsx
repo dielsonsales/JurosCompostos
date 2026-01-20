419d262794a884fd6a486bcd98403d0cfd84e450
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-20 15:35:57 -0300

    Create initial text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <main>
-      </main>
-    </>
+    <main>
+      <div>
+        <h1>Juros Compostos</h1>
+        <p>Simule a evolução do seu capital considerando aportes mensais e uma taxa recorrente.</p>
+      </div>
+    </main>
   );
 }
