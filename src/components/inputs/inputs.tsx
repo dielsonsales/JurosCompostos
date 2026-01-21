d6fd4b92d36b821fb95d488ccab7e7e7a6019104
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-21 00:38:44 -0300

    Attempt at styling the dropdowns

--- src/components/inputs/inputs.tsx
+++ src/components/inputs/inputs.tsx
@@ -8,9 +8,21 @@ export default function Inputs() {
       <p className="defaultText">Aporte mensal (R$)</p>
       <input type="text" inputMode="decimal" className={styles.input} />
       <p className="defaultText">Taxa (%)</p>
-      <input type="text" inputMode="decimal" className={styles.input} />
+      <div className={styles.dropDownContainer}>
+        <select className="dropDown">
+          <option value="mensal">Mensal</option>
+          <option value="anual">Anual</option>
+        </select>
+        <input type="text" inputMode="decimal" className={styles.input} />
+      </div>
       <p className="defaultText">Período</p>
-      <input type="text" inputMode="decimal" className={styles.input} />
+      <div className={styles.dropDownContainer}>
+        <select className="dropDown">
+          <option value="meses">Meses</option>
+          <option value="anos">Anos</option>
+        </select>
+        <input type="text" inputMode="decimal" className={styles.input} />
+      </div>
     </div>
   );
 }
