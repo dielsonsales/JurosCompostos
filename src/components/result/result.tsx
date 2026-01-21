fbb4633094a07e2cef709fef78ef149b6bf88cb1
Dielson Sales de Carvalho <dielsonscarvalho@gmail.com>
2026-01-20 21:54:59 -0300

    Add style to other texts

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -4,9 +4,9 @@ export default function Result() {
   return (
     <div>
       <p className="subTitle">Resultado estimado</p>
-      <p>R$ 4.158,98</p>
-      <p>Total aportado: R$ 3.900,00</p>
-      <p>Juros acumulados: R$258,98</p>
+      <p className="title">R$ 4.158,98</p>
+      <p className="defaultText">Total aportado: R$ 3.900,00</p>
+      <p className="defaultText">Juros acumulados: R$258,98</p>
     </div>
   );
 }
